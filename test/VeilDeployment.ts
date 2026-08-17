1a180de09fc938c216618c4c593f8f820c8d4856
Iniwura <114838709+Iniwura@users.noreply.github.com>
2026-08-17 10:01:59 +0100

    Test VEIL deployment wiring

---
+++ test/VeilDeployment.ts
@@ -0,0 +1,26 @@
+import { expect } from "chai";
+import { deployments, ethers } from "hardhat";
+
+describe("VEIL deployment", function () {
+  beforeEach(async function () {
+    await deployments.fixture(["VEIL"]);
+  });
+
+  it("deploys and wires the confidential custody stack", async function () {
+    const asset = await deployments.get("MockConfidentialToken");
+    const poolDeployment = await deployments.get("VeilPool");
+    const yieldDeployment = await deployments.get("VeilYieldSource");
+    const vaultDeployment = await deployments.get("VeilPrizeVault");
+
+    const pool = await ethers.getContractAt("VeilPool", poolDeployment.address);
+    const yieldSource = await ethers.getContractAt("VeilYieldSource", yieldDeployment.address);
+    const prizeVault = await ethers.getContractAt("VeilPrizeVault", vaultDeployment.address);
+
+    expect(await pool.asset()).to.equal(asset.address);
+    expect(await yieldSource.asset()).to.equal(asset.address);
+    expect(await yieldSource.prizeVault()).to.equal(vaultDeployment.address);
+    expect(await prizeVault.pool()).to.equal(poolDeployment.address);
+    expect(await prizeVault.asset()).to.equal(asset.address);
+    expect(await prizeVault.yieldSource()).to.equal(yieldDeployment.address);
+  });
+});
