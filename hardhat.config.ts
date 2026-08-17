ef17b1fce752a1683b375b72a85a946aa81d2eb1
Iniwura <114838709+Iniwura@users.noreply.github.com>
2026-08-17 10:02:11 +0100

    Make Sepolia RPC configurable

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,10 +12,13 @@ import "solidity-coverage";
 import "./tasks/accounts";
 import "./tasks/FHECounter";
 
-// Run 'npx hardhat vars setup' to see the list of variables that need to be set
-
+// Run 'npx hardhat vars setup' to see the list of variables that need to be set.
 const MNEMONIC: string = vars.get("MNEMONIC", "test test test test test test test test test test test junk");
 const INFURA_API_KEY: string = vars.get("INFURA_API_KEY", "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+const SEPOLIA_RPC_URL: string = vars.get(
+  "SEPOLIA_RPC_URL",
+  `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+);
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -55,7 +58,7 @@ const config: HardhatUserConfig = {
         count: 10,
       },
       chainId: 11155111,
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      url: SEPOLIA_RPC_URL,
     },
   },
   paths: {
