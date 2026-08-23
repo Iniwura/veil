df53cbeb25abc6df1193747afb25b52da35f9fc9
Iniwura <114838709+Iniwura@users.noreply.github.com>
2026-08-23 23:30:31 +0100

    Mount UNVEIL live notifications

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import ErrorBoundary from "./ErrorBoundary";
+import NotificationCenter from "./NotificationCenter";
 import "./styles.css";
 import "./unveil-motion.css";
+import "./notifications.css";
 import "./error-boundary.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ErrorBoundary>
       <App />
+      <NotificationCenter />
     </ErrorBoundary>
   </React.StrictMode>,
 );
