1fde9a40def3d2483484e346a8a14b0586353682
Iniwura <114838709+Iniwura@users.noreply.github.com>
2026-08-23 16:01:56 +0100

    Test sequential yield-round wiring

--- test/VeilDeployment.ts
+++ test/VeilDeployment.ts
@@ -21,7 +21,9 @@ describe("UNVEIL deployment", function () {
     expect(await pool.drawPeriod()).to.be.greaterThan(0);
     expect(await pool.nextDrawClosesAt()).to.be.greaterThan(0);
     expect(await yieldSource.asset()).to.equal(asset.address);
+    expect(await yieldSource.pool()).to.equal(poolDeployment.address);
     expect(await yieldSource.strategyOperator()).to.equal(deployer.address);
+    expect(await yieldSource.yieldRoundId()).to.equal(1);
     expect(await yieldSource.prizeVault()).to.equal(vaultDeployment.address);
     expect(await prizeVault.pool()).to.equal(poolDeployment.address);
     expect(await prizeVault.asset()).to.equal(asset.address);
